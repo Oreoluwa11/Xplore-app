c7adcde23a365c60f6c283744fad3628e821f047
Rahmat-FI <rahmatanoba1@gmail.com>
2025-12-15 21:09:31 +0100

    fixes

--- app/api/restaurants/[id]/route.ts
+++ app/api/restaurants/[id]/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { readFile } from "fs/promises";
 import path from "path";
 
